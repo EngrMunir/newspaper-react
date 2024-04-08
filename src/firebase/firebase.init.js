a6bab46add95bdb5db000577e3c54e44ab2f9393
Munir <smsirajulmonir@gmail.com>
2024-04-08 16:36:21 +0600

    basic setup ready

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb_A1Uiajwcxhx8EF6ldsEEExVmgkPbh4",
+  authDomain: "newspaper-react-8214d.firebaseapp.com",
+  projectId: "newspaper-react-8214d",
+  storageBucket: "newspaper-react-8214d.appspot.com",
+  messagingSenderId: "384285079804",
+  appId: "1:384285079804:web:a0931d231929b96e5dff79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
